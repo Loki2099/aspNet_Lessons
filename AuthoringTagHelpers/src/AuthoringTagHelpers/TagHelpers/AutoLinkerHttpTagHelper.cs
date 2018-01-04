15e441a2461a26e752ec0a942291a51a1552e085
Loki2099 <Mark.Satterwhite2099@gmail.com>
2018-01-04 11:27:15 -0600

    Tag Helpers

---
+++ AuthoringTagHelpers/src/AuthoringTagHelpers/TagHelpers/AutoLinkerHttpTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.RegularExpressions;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+	[HtmlTargetElement("p")]
+	public class AutoLinkerHttpTagHelper : TagHelper
+	{
+		public override int Order
+		{
+			get{return int.MinValue; }
+			//get { return int.MinValue; }
+		}
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			//var childContent = await output.GetChildContentAsync();
+			//output.Content.SetHtmlContent(Regex.Replace(childContent.GetContent(), @"\b(?:https?://)(\S+)\b", "<a target=\"_blank\" href=\"$0\">$0</a>"));
+
+			var childContent = output.Content.IsModified ? output.Content.GetContent() : (await output.GetChildContentAsync()).GetContent();
+			output.Content.SetHtmlContent(Regex.Replace(childContent, @"\b(?:https?://)(\S+)\b", "<a target=\"_blank\" href=\"$0\">$0</a>"));
+		}
+	}
+
+
+	[HtmlTargetElement("p")]
+	public class AutoLinkerWwwTagHelper : TagHelper
+	{
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			//var childContent = await output.GetChildContentAsync();
+			//output.Content.SetHtmlContent(Regex.Replace(childContent.GetContent(), @"\b(www\.)(\S+)\b", "<a target=\"_blank\" href=\"http://$0\">$0</a>"));
+
+			var childContent = output.Content.IsModified ? output.Content.GetContent() : (await output.GetChildContentAsync()).GetContent();
+			output.Content.SetHtmlContent(Regex.Replace(childContent, @"\b(www\.)(\S+)\b", "<a target=\"_blank\" href=\"http://$0\">$0</a>"));
+		}
+	}
+}
---
+++ AuthoringTagHelpers/src/AuthoringTagHelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+	public class EmailTagHelper : TagHelper
+	{
+		private const string EmailDomain = "contoso.com";
+		//public string MailTo { get; set; }
+		//public override void Process(TagHelperContext context, TagHelperOutput output)
+		//{
+		//	output.TagName = "a";
+		//	var address = MailTo + "@" + EmailDomain;
+		//	output.Attributes.SetAttribute("href", "mailto:" + address);
+		//	output.Content.SetContent(address);
+		//}
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			var content = await output.GetChildContentAsync();
+			var target = content.GetContent() + "@" + EmailDomain;
+			output.Attributes.SetAttribute("href", "mailto:" + target);
+			output.Content.SetContent(target);
+		}
+	}
+}
