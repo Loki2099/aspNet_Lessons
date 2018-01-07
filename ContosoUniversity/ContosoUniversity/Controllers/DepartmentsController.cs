ec64714719c6d36bcae129f61b6400712a3c3b12
Loki2099 <33090971+Loki2099@users.noreply.github.com>
2018-01-07 12:38:22 -0600

    Updated Branch Code

--- ContosoUniversity/ContosoUniversity/Controllers/DepartmentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/DepartmentsController.cs
@@ -34,11 +34,16 @@ namespace ContosoUniversity.Controllers
                 return NotFound();
             }
 
-            var department = await _context.Departments
-                .Include(d => d.Administrator)
+			string query = "SELECT * FROM Department WHERE DepartmentID = {0}";
+
+			var department = await _context.Departments
+				.FromSql(query, id)
+				.Include(d => d.Administrator)
 				.AsNoTracking()
-				.SingleOrDefaultAsync(m => m.DepartmentID == id);
-            if (department == null)
+				.SingleOrDefaultAsync();
+				//.SingleOrDefaultAsync(m => m.DepartmentID == id);
+
+			if (department == null)
             {
                 return NotFound();
             }
